79f2af298394eaeacaa568b589e419f4323e5059
Mars4me <lmarsl1994@gmail.com>
2023-08-07 03:04:57 +0300

    update components

--- src/UI/Stack/index.tsx
+++ src/UI/Stack/index.tsx
@@ -1,15 +1,20 @@
+'use client';
+
+import { cn } from '@/utils/cn';
 import React, { ReactNode } from 'react';
 
 interface StackProps {
     children?: ReactNode;
+    className?: string;
 }
 
-const Stack = ({ children }: StackProps) => {
+const Stack = ({ children, className = '' }: StackProps) => {
     return (
         <div
-            className={
-                'grid w-full pt-8 pb-6 mt-4 text-center border-gray-300 md:mt-0 lg:mb-0 rounded-lg sm:grid-cols-3 lg:grid-cols-5 lg:text-left bg-gradient-to-b dark:shadow-inner dark:shadow-purple-900 from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'
-            }
+            className={cn(
+                'grid w-full pt-8 pb-6 mt-4 text-center border-gray-300 md:mt-0 lg:mb-0 sm:grid-cols-3 lg:grid-cols-5 lg:text-left bg-gradient-to-b dark:shadow-inner dark:shadow-purple-900 from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit md:static lg:w-auto lg:rounded-xl md:border md:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30',
+                className
+            )}
         >
             {children}
         </div>
